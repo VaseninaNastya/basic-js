3b255f886f02f58ed1cf0fd3ff3286300a2d7534
NastyaVasenina <amedeo_@mail.ru>
2020-09-26 17:15:07 +0300

    feat: add count-cats and dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function countCats(matrix) {
+  let arr2 = [];
+for (i = 0; i < matrix.length; i++) {
+  for (j = 0; j < matrix[i].length; j++) {
+    arr2.push(matrix[i][j]);
+  }
+}
+let catArr = arr2.filter(function(number) {
+  if (number =='^^'){
+    return number
+  }
+});
+  return catArr.length
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)){return false}
+  let arr = members.map(function(name){
+if(typeof name == "string"){
+  if(!(name.charAt(0) === " ")){
+    return name.slice(0, 1).toUpperCase()
+  }else{
+    let notEmptyName = name
+    while(notEmptyName.charAt(0) === " "){
+      notEmptyName = notEmptyName.slice(1,notEmptyName.length)
+    }return notEmptyName.slice(0, 1).toUpperCase()
+  }
+}
+  })
+
+
+  return arr.sort().join('')
+}
